af71079a7a9df00fd9b38f8a248b36b6ffcd0342
Christoph Dorn <christoph@christophdorn.com>
2017-05-27 00:33:31 -0700

    initil

---
+++ Projects/2017_05-Website/main.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash.origin.script
+
+depend {
+    "pages": "@com.github/pinf-to/to.pinf.com.github.pages#s1"
+}
+
+CALL_pages publish {
+    "anchors": {
+        "body": (javascript () >>>
+
+//TODO: Scan for ... `### Start Your Own Node` and locate "```javascript" to run code in nodejs
+
+
+            return "Hello World!";
+        <<<)
+    },    
+    "css": (css () >>>
+        BODY {
+            width: 80%;
+            margin-left: 10%;
+            padding: 0px;
+            padding-top: 30px;
+            padding-bottom: 30px;
+        }
+
+        BODY > P {
+            margin-left: 10px;
+            margin-right: 10px;
+        }
+
+        A {
+            color: #0000ff;
+            text-decoration: none;
+            border-bottom: 1px dashed;
+        }
+
+        A:visited {
+            color: #aa5e13;
+            border-bottom: 1px solid #ffffff;
+        }
+
+        H1 {
+            border-bottom: 3px solid #1f75af;
+            padding-bottom: 10px;
+            padding-left: 10px;
+        }
+
+        H2 {
+            border-bottom: 2px solid rgba(31,117,175,0.6);
+            padding-bottom: 5px;
+            padding-left: 10px;
+        }
+
+        H3 {
+            border-bottom: 1px solid rgba(32,117,175,0.4);
+            padding-left: 10px;
+        }
+
+        PRE {
+            background-color: #fcfcfc;    
+            border: 1px solid #dcdcdc;
+            padding: 10px;
+            margin-left: 10px;
+            margin-right: 10px;
+        }
+
+        CODE {
+            font: 12px "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
+        }
+
+        TABLE {
+            WIDTH: 100%;
+            margin-left: 10px;
+            margin-right: 10px;
+        }
+    <<<)
+}
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# beatgit
-Status Page
+
+| License | Source | &#8212;&raquo; | [Website](https://github.com/beatgit/beatgit/blob/master/Projects/2017_05-Website/main.sh) | [npm](https://github.com/npm/npm) |
+| :---: | :---: | :---: | :---: | :---: |
+| [FPL](https://opensource.org/licenses/FPL-1.0.0) | [github.com/beatgit/beatgit](https://github.com/beatgit/beatgit) | [![CircleCI](https://circleci.com/gh/beatgit/beatgit.svg?style=svg)](https://circleci.com/gh/beatgit/beatgit) | [beatgit.github.io/beatgit](https://beatgit.github.io/beatgit) | `beatgit`
+
+beatgit
+=======
+
+A Bash and [NodeJS](https://nodejs.org/) Toolchain that helps you Leverage [git](https://git-scm.com/) at a more Abstract Level for a Seamless Workflow with Others.
+
+Usage
+-----
+
+```javascript
+const BEATGIT = require("beatgit");
+```
+
+### Start Your Own Node
+
+```javascript
+BEATGIT.rise({
+    "pkey": "<NodeJS compatible Private Key>",
+    "attach": "github.com/${GITHUB_USER}/${GITHUB_REPOSITORY}"
+}).then(function (TRUNK) {
+
+    // trunk.id ~ Public Globally Unique ID (Public Key based)
+    // trunk.connect ~ Abstract Object to handle Communication
+    // trunk.fs ~ Globally Distributed Encrypted Filesystem
+
+    return TRUNK.connect.send(
+        "https://github.com/beatgit/beatgit.git",
+        TRUNK.fs("${__DIRNAME__}")
+    });
+}).catch(console.error);
+```
+
+Provenance
+==========
+
+Original Source Logic under [Free Public License](https://opensource.org/licenses/FPL-1.0.0) by [Christoph Dorn](http://christophdorn.com) since 2017.
